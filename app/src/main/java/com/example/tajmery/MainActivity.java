35406d5c5497e81605f757a91b42f072a056e421
piotr <piotr.zawadzki0@tlen.pl>
2025-06-26 09:21:54 +0200

    added stoper project

--- app/src/main/java/com/example/tajmery/MainActivity.java
+++ app/src/main/java/com/example/tajmery/MainActivity.java
@@ -2,14 +2,40 @@ package com.example.tajmery;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView stoper1, stoper2;
+    private long startTime1 = 0, startTime2 = 0;
+    private boolean running1 = false, running2 = false;
+    private Handler handler = new Handler();
+
+    private Runnable updateStoper1 = new Runnable() {
+        @Override
+        public void run() {
+            if (running1) {
+                long elapsed = System.currentTimeMillis() - startTime1;
+                stoper1.setText(formatTime(elapsed));
+                handler.postDelayed(this, 10);
+            }
+        }
+    };
+
+    private Runnable updateStoper2 = new Runnable() {
+        @Override
+        public void run() {
+            if (running2) {
+                long elapsed = System.currentTimeMillis() - startTime2;
+                stoper2.setText(formatTime(elapsed));
+                handler.postDelayed(this, 10);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,39 +43,58 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        stoper1 = findViewById(R.id.stoper1);
+        stoper2 = findViewById(R.id.stoper2);
+    }
+
+    public void startStoper1(View view) {
+        if (!running1) {
+            startTime1 = System.currentTimeMillis() - getTimeFromText(stoper1);
+            running1 = true;
+            handler.post(updateStoper1);
+        }
+    }
+
+    public void stopStoper1(View view) {
+        running1 = false;
+    }
+
+    public void resetStoper1(View view) {
+        running1 = false;
+        stoper1.setText("00:00:00.00");
+    }
+
+    public void startStoper2(View view) {
+        if (!running2) {
+            startTime2 = System.currentTimeMillis() - getTimeFromText(stoper2);
+            running2 = true;
+            handler.post(updateStoper2);
+        }
+    }
+
+    public void stopStoper2(View view) {
+        running2 = false;
+    }
+
+    public void resetStoper2(View view) {
+        running2 = false;
+        stoper2.setText("00:00:00.00");
+    }
+
+    private String formatTime(long millis) {
+        long minutes = (millis / 60000) % 60;
+        long seconds = (millis / 1000) % 60;
+        long millisecs = (millis % 1000) / 10;
+        return String.format("%02d:%02d:%02d.%02d", 0, minutes, seconds, millisecs);
+    }
+
+    private long getTimeFromText(TextView textView) {
+        String[] parts = textView.getText().toString().split(":|\\.");
+        return Integer.parseInt(parts[1]) * 60000 + Integer.parseInt(parts[2]) * 1000 + Integer.parseInt(parts[3]) * 10;
     }
 
     public void onClickButton(View view) {
         Intent intent = new Intent(this, MainActivitykolejne.class);
         startActivity(intent);
-    }
-
-    public void startStoper1(View view) {
-
-    }
-
-    public void stopStoper1(View view) {
-
-    }
-
-    public void resetStoper1(View view) {
-
-    }
-
-    public void startStoper2(View view) {
-
-    }
-
-    public void stopStoper2(View view) {
-
-    }
-
-    public void resetStoper2(View view) {
-
     }
 }
--- app/src/main/java/com/example/tajmery/MainActivitykolejne.java
+++ app/src/main/java/com/example/tajmery/MainActivitykolejne.java
@@ -2,45 +2,109 @@ package com.example.tajmery;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivitykolejne extends AppCompatActivity {
+
+    private TextView minutnik1, minutnik2;
+    private CountDownTimer timer1, timer2;
+    private boolean running1 = false, running2 = false;
+    private long timeLeft1 = 60000, timeLeft2 = 60000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.tojestactivityinne);
+        // setContentView(R.layout.activity_main_kolejne);
+        minutnik1 = findViewById(R.id.minutnik1);
+        minutnik2 = findViewById(R.id.minutnik2);
+
+        updateTimerText(minutnik1, timeLeft1);
+        updateTimerText(minutnik2, timeLeft2);
+    }
+
+    public void startMinutnik1(View view) {
+        if (!running1) {
+            timer1 = new CountDownTimer(timeLeft1, 10) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    timeLeft1 = millisUntilFinished;
+                    updateTimerText(minutnik1, timeLeft1);
+                }
+
+                @Override
+                public void onFinish() {
+                    running1 = false;
+                }
+            }.start();
+            running1 = true;
+        }
+    }
+
+    public void stopMinutnik1(View view) {
+        if (running1) {
+            timer1.cancel();
+            running1 = false;
+        }
+    }
+
+    public void resetMinutnik1(View view) {
+        if (running1) {
+            timer1.cancel();
+            running1 = false;
+        }
+        timeLeft1 = 60000;
+        updateTimerText(minutnik1, timeLeft1);
+    }
+
+    public void startMinutnik2(View view) {
+        if (!running2) {
+            timer2 = new CountDownTimer(timeLeft2, 10) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    timeLeft2 = millisUntilFinished;
+                    updateTimerText(minutnik2, timeLeft2);
+                }
+
+                @Override
+                public void onFinish() {
+                    running2 = false;
+                }
+            }.start();
+            running2 = true;
+        }
+    }
+
+    public void stopMinutnik2(View view) {
+        if (running2) {
+            timer2.cancel();
+            running2 = false;
+        }
+    }
+
+    public void resetMinutnik2(View view) {
+        if (running2) {
+            timer2.cancel();
+            running2 = false;
+        }
+        timeLeft2 = 60000;
+        updateTimerText(minutnik2, timeLeft2);
+    }
+
+    private void updateTimerText(TextView textView, long millis) {
+        long minutes = (millis / 60000) % 60;
+        long seconds = (millis / 1000) % 60;
+        long millisecs = (millis % 1000) / 10;
+        textView.setText(String.format("%02d:%02d:%02d.%02d", 0, minutes, seconds, millisecs));
     }
 
     public void goToMain(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-    }
-
-    public void startMinutnik1(View view) {
-
-    }
-
-    public void stopMinutnik1(View view) {
-
-    }
-
-    public void resetMinutnik1(View view) {
-
-    }
-
-    public void startMinutnik2(View view) {
-
-    }
-
-    public void stopMinutnik2(View view) {
-
-    }
-
-    public void resetMinutnik2(View view) {
-
     }
 }
